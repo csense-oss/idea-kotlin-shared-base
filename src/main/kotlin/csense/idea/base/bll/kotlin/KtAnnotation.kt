438ae5b89e47f540a5be0e7660f1e0f91c610988
Kasper T <kasper@webmasteren.eu>
2023-02-12 15:56:16 +0100

    more split

--- src/main/kotlin/csense/idea/base/bll/kotlin/KtAnnotation.kt
+++ src/main/kotlin/csense/idea/base/bll/kotlin/KtAnnotation.kt
@@ -1,8 +1,9 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "CANDIDATE_CHOSEN_USING_OVERLOAD_RESOLUTION_BY_LAMBDA_ANNOTATION")
 
 package csense.idea.base.bll.kotlin
 
 import com.intellij.openapi.project.Project
+import csense.idea.base.*
 import csense.idea.base.bll.psiWrapper.`class`.*
 import csense.idea.base.bll.psiWrapper.`class`.operations.*
 import csense.kotlin.extensions.collections.*
@@ -19,13 +20,15 @@ fun List<KtAnnotationEntry>.filterThrowsAnnotations(): List<KtAnnotationEntry> =
 
 fun List<KtAnnotationEntry>.resolveAsThrowTypes(
     project: Project
-): List<KtPsiClass> {
-    return map { it: KtAnnotationEntry ->
-        it.valueArguments.mapNotNull { annotation: ValueArgument ->
-            //TODO. if for some cool reason the code contains "arrayOf" or alike (with star operator etc) then this will fail badly.
-            annotation.getArgumentExpression()?.resolveFirstClassType2()
-        }
-    }.flatten().nullOnEmpty() ?: listOfNotNull(
+): List<KtPsiClass> = resolveClassTypes().onEmpty {
+    listOfNotNull(
         KtPsiClass.getKotlinThrowable(project)
     )
 }
+
+fun List<KtAnnotationEntry>.resolveClassTypes(): List<KtPsiClass> = map { it: KtAnnotationEntry ->
+    it.valueArguments.mapNotNull { annotation: ValueArgument ->
+        //TODO. if for some cool reason the code contains "arrayOf" or alike (with star operator etc) then this will fail badly.
+        annotation.getArgumentExpression()?.resolveFirstClassType2()
+    }
+}.flatten()
--- src/main/kotlin/csense/idea/base/csense.kt
+++ src/main/kotlin/csense/idea/base/csense.kt
@@ -1,6 +1,10 @@
-@file:Suppress("NOTHING_TO_INLINE")
+@file:Suppress("NOTHING_TO_INLINE", "unused")
+@file:OptIn(ExperimentalContracts::class)
 
 package csense.idea.base
+
+import kotlin.contracts.*
+import kotlin.experimental.*
 
 public inline fun Collection<*>.isLastIndex(
     index: Int
@@ -8,9 +12,24 @@ public inline fun Collection<*>.isLastIndex(
     return size - 1 == index
 }
 
-public inline fun <T> Collection<T>.onEmpty(
-    otherCollection: Collection<T>
-): Collection<T> = when (isEmpty()) {
+@OptIn(ExperimentalTypeInference::class)
+@OverloadResolutionByLambdaReturnType
+public inline fun <T, CollectionType : Collection<T>> CollectionType.onEmpty(
+    otherCollection: CollectionType
+): CollectionType = when (isEmpty()) {
     true -> otherCollection
     false -> this
 }
+
+public inline fun <T, CollectionType : Collection<T>> CollectionType.onEmpty(
+    action: () -> CollectionType
+): CollectionType {
+    contract {
+        callsInPlace(action, InvocationKind.AT_MOST_ONCE)
+    }
+    return when (isEmpty()) {
+        true -> action()
+        false -> this
+    }
+}
+
