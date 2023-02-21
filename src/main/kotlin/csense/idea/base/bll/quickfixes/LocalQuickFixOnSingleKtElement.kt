294a0217e431074c072ad00b62ba91e94a97d501
Kasper T <kasper@webmasteren.eu>
2023-02-21 22:50:42 +0100

    add some more base classes

---
+++ src/main/kotlin/csense/idea/base/bll/quickfixes/LocalQuickFixOnSingleKtElement.kt
@@ -0,0 +1,19 @@
+package csense.idea.base.bll.quickfixes
+
+import com.intellij.codeInspection.*
+import com.intellij.openapi.project.*
+import com.intellij.psi.*
+import org.jetbrains.kotlin.psi.*
+
+abstract class LocalQuickFixOnSingleKtElement<T : KtElement>(
+    element: T
+) : LocalQuickFixOnPsiElement(element) {
+
+    @Suppress("UNCHECKED_CAST")
+    final override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
+        val elementToUse: T = startElement as? T ?: return
+        invoke(project = project, file = file, element = elementToUse)
+    }
+
+    abstract fun invoke(project: Project, file: PsiFile, element: T)
+}
---
+++ src/main/kotlin/csense/idea/base/bll/quickfixes/LocalQuickFixUpdateCode.kt
@@ -0,0 +1,40 @@
+@file:Suppress("unused")
+
+package csense.idea.base.bll.quickfixes
+
+import com.intellij.openapi.project.*
+import com.intellij.psi.*
+import com.intellij.psi.codeStyle.*
+import org.jetbrains.kotlin.idea.util.application.*
+import org.jetbrains.kotlin.psi.*
+
+
+abstract class LocalQuickFixUpdateCode<T : KtElement>(
+    element: T
+) : LocalQuickFixOnSingleKtElement<T>(element) {
+
+    final override fun invoke(project: Project, file: PsiFile, element: T) {
+        if (!element.isWritable) {
+            return
+        }
+        val updatedElement: PsiElement = project.executeWriteCommand(
+            name = this::class.simpleName ?: name,
+            groupId = familyName
+        ) {
+            tryUpdate(
+                project = project,
+                file = file,
+                element = element
+            )
+        } ?: return
+        reformat(project = project, element = updatedElement)
+    }
+
+    abstract fun tryUpdate(project: Project, file: PsiFile, element: T): PsiElement?
+
+    fun reformat(project: Project, element: PsiElement): PsiElement {
+        val styleManager: CodeStyleManager = CodeStyleManager.getInstance(project)
+        return styleManager.reformat(/* element = */ element)
+    }
+
+}
