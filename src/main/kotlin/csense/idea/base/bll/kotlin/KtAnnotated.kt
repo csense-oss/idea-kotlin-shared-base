9503b8d1c4549751c01012dcd5199f9eb6e7401d
Kasper T <kasper@webmasteren.eu>
2023-03-18 14:09:04 +0100

    Update KtAnnotated.kt

--- src/main/kotlin/csense/idea/base/bll/kotlin/KtAnnotated.kt
+++ src/main/kotlin/csense/idea/base/bll/kotlin/KtAnnotated.kt
@@ -4,12 +4,42 @@ package csense.idea.base.bll.kotlin
 
 import com.intellij.openapi.project.*
 import csense.idea.base.bll.psiWrapper.`class`.*
+import csense.idea.base.bll.psiWrapper.`class`.operations.*
+import org.jetbrains.kotlin.idea.kdoc.*
+import org.jetbrains.kotlin.idea.util.*
+import org.jetbrains.kotlin.kdoc.parser.*
+import org.jetbrains.kotlin.kdoc.psi.api.*
+import org.jetbrains.kotlin.kdoc.psi.impl.*
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.*
 
 fun KtAnnotated.throwsAnnotationOrNull(): KtAnnotationEntry? =
     annotationEntries.filterThrowsAnnotation()
 
 fun KtAnnotated.throwsTypes(): List<KtPsiClass> {
+    val fromDocumentation: List<KtPsiClass> = throwsDocumentationOrNull()
+    val fromAnnotations: List<KtPsiClass> = throwsAnntationTypes()
+    return fromDocumentation + fromAnnotations
+}
+
+fun KtAnnotated.throwsAnntationTypes(): List<KtPsiClass> {
     val annotations: KtAnnotationEntry = throwsAnnotationOrNull() ?: return emptyList()
     return listOf(annotations).resolveAsThrowTypesOrThrowable(project)
+}
+
+fun KtAnnotated.throwsDocumentationOrNull(): List<KtPsiClass> {
+
+    val docsThrows = "@throws"
+    val kdocs: List<KDoc> = collectDescendantsOfType()
+    val kdocsText: List<String> = kdocs.map { it: KDoc ->
+        it.text
+    }
+    val throwsDocsText: List<String> = kdocsText.filter { it.contains(docsThrows) }
+    val classNames: List<String> = throwsDocsText.map { it: String -> //todo this is kinda sub optimal (kinda sus)
+        it.substringAfter(docsThrows).trim().substringBefore(" ").trim()
+    }
+    //TODO kotlin in front? hmmmmmmmmm
+    return classNames.mapNotNull { className: String ->
+        KtPsiClass.resolve(fqName = className, project = project) ?: KtPsiClass.resolve(fqName = "kotlin.$className", project = project)
+    }
 }
