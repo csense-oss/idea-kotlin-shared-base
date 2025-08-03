fefacdcd3c3b42e6890923353ae372415d58a397
Kasper Tvede <kasper@webmasteren.eu>
2025-08-03 09:48:55 +0200

    pr

--- src/main/kotlin/csense/idea/base/bll/kotlin/KtThrowExpression.kt
+++ src/main/kotlin/csense/idea/base/bll/kotlin/KtThrowExpression.kt
@@ -4,10 +4,9 @@ package csense.idea.base.bll.kotlin
 
 import csense.idea.base.bll.psiWrapper.`class`.*
 import csense.idea.base.bll.psiWrapper.`class`.operations.*
-import org.jetbrains.kotlin.analysis.api.analyze
-import org.jetbrains.kotlin.analysis.api.types.symbol
+import org.jetbrains.kotlin.analysis.api.*
+import org.jetbrains.kotlin.analysis.api.types.*
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.types.*
 
 fun KtThrowExpression.resolveThrownTypeOrNull(): KtPsiClass? {
 
@@ -23,8 +22,6 @@ fun KtThrowExpression.tryResolveViaExpressionType(): KtPsiClass? {
     val fqName: String = analyze(expression){
         expression.expressionType?.symbol?.classId?.asString()
     } ?: return null
-//    val resolvedExpressionType: KotlinType = thrownExpression?.resolveExpressionType() ?: return null
-//    val fqName: String = resolvedExpressionType.fqNameAsString() ?: return null
     return KtPsiClass.resolveByKotlin(fqName = fqName, project = project)
         ?: KtPsiClass.resolveByJava(fqName = fqName, project = project)
 }
