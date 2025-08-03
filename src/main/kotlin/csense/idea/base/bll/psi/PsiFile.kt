ba87fa0dc6e5e76fe6eb6ad83e3adf9b9081e69f
Kasper Tvede <kasper@webmasteren.eu>
2025-08-03 09:40:47 +0200

    Update PsiFile.kt

--- src/main/kotlin/csense/idea/base/bll/psi/PsiFile.kt
+++ src/main/kotlin/csense/idea/base/bll/psi/PsiFile.kt
@@ -2,7 +2,13 @@ package csense.idea.base.bll.psi
 
 import com.intellij.openapi.module.*
 import com.intellij.psi.*
+import csense.idea.base.bll.kotlin.isStubbed
+import org.jetbrains.kotlin.psi.KtFile
 
 fun PsiFile.findModule(): Module? {
     return ModuleUtil.findModuleForFile(this)
+}
+
+fun PsiFile.isKtFileStubbed(): Boolean {
+    return this is KtFile && this.isStubbed()
 }
